b4748df23446041824ef4cc903d14a73cde43775
soares-roydon <soaresroydon@gmail.com>
2026-08-26 17:33:27 +0530

    Connect the frontend and backend

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'htpp://localhost:3000'
+    }
+  }
 })
