41e7bb63833d4d5aee34615bca40c964af7d0b26
soares-roydon <soaresroydon@gmail.com>
2026-08-28 14:54:44 +0530

    Add cors

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
+import cors from 'cors';
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(
+   cors({
+      origin: 'https://roychat.vercel.app',
+   })
+);
 app.use(express.json());
 app.use('/api', router);
 
