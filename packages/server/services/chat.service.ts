9d04e029e0b86fa83c1f7034638710a1466f4475
soares-roydon <soaresroydon@gmail.com>
2026-08-30 11:36:56 +0530

    fix dir resolution in es

--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import { GoogleGenAI } from '@google/genai';
 import { conversationRepository } from '../repositories/conversation.repository.js';
-import { join } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Implementation detail
 const ai = new GoogleGenAI({
@@ -19,8 +20,11 @@ type ChatResponseType = {
 // );
 // const instructions = template.replace('{{RULES}}', rules);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const instructions = fs.readFileSync(
-   join(__dirname, '..', 'prompts', 'chatbot.txt'),
+   path.join(__dirname, '..', 'prompts', 'chatbot.txt'),
    'utf-8'
 );
 
